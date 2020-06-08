9e33ff6235c148c55ef183b0219d8f71cbfda6e8
Manuel Moauro <manuelmoauro1@gmail.com>
2020-06-08 18:25:26 -0300

    Agregado en la busqueda por pais, ahora se puede buscar con el nombre del pais.

--- js/main.js
+++ js/main.js
@@ -5,15 +5,15 @@ function iniciarPagina() {
     document.getElementById("obtenerSegunUser").addEventListener("click", obtenerPaisUser);
 
     const url = "https://api.covid19api.com/summary";
+    const parrafoTexto = document.getElementById("texto");
     
     async function obtenerGlobales() {
-        document.getElementById("texto").innerHTML = "Obteniendo datos..."
+        document.getElementById("texto").innerHTML = "Obteniendo datos...";
         
         try {
             let response = await fetch(url);
             if (response.ok) {
                 let json = await response.json();
-                document.getElementById("texto").innerHTML = " ";
                 document.getElementById("globalNuevosCasos").innerHTML = json.Global.NewConfirmed;
                 document.getElementById("globalTotalCasos").innerHTML = json.Global.TotalConfirmed;
                 document.getElementById("globalNuevasMuertes").innerHTML = json.Global.NewDeaths;
@@ -24,19 +24,18 @@ function iniciarPagina() {
             }
         }
         catch {
-            document.getElementById("texto").innerHTML = "Error al obtener los datos."
+            parrafoTexto.innerHTML = "Error al obtener los datos. Intente de nuevo mas tarde.";
         }
     }
 
     async function obtenerPaisUser() {
         const inputUser1 = document.getElementById("inputUser").value;
         const inputUser = inputUser1.toUpperCase();
-        document.getElementById("texto").innerHTML = "Obteniendo los datos"
+        parrafoTexto.innerHTML = "Obteniendo los datos...";
         if (inputUser.length == 2) {
             try {
                 let response = await fetch(url);
                 if(response.ok) {
-                    document.getElementById("texto").innerHTML = " "
                     let json = await response.json();
                     for(let i = 0; i < json.Countries.length; i++) {
                         if (json.Countries[i].CountryCode == inputUser) {
@@ -77,16 +76,68 @@ function iniciarPagina() {
                             document.getElementById("alignDiv").appendChild(espaciado);
                         }
                     }  
-                    document.getElementById("texto").innerHTML = "Ultima actualizacion: " + json.Date;
+                    parrafoTexto.innerHTML = "Ultima actualizacion: " + json.Date;
                 }
             }
             catch {
-                document.getElementById("texto").innerHTML = "Error al obtener los datos." 
+                parrafoTexto.innerHTML = "Error al obtener los datos. Intente de nuevo mas tarde" ;
             }
         }
+        else if(inputUser1.length > 2) {
+            try {
+                let response = await fetch(url);
+                if(response.ok) {
+                    let json = await response.json();
+                    for(let i = 0; i < json.Countries.length; i++) {
+                        if (json.Countries[i].Country == inputUser1) {
+                            let newDiv = document.createElement("div");
+                            newDiv.id = "alignDiv";
+                            document.getElementById("datosUser").appendChild(newDiv);
+
+                            let pais = document.createElement("h2");
+                            pais.innerHTML = json.Countries[i].Country;
+                            document.getElementById("alignDiv").appendChild(pais);
+
+                            let newCC = document.createElement("p");
+                            newCC.innerHTML = "Nuevos casos confirmados: " + json.Countries[i].NewConfirmed;
+                            document.getElementById("alignDiv").appendChild(newCC);
+    
+                            let newTC = document.createElement("p");
+                            newTC.innerHTML = "Total casos confirmados: " + json.Countries[i].TotalConfirmed;
+                            document.getElementById("alignDiv").appendChild(newTC);
+    
+                            let newDC = document.createElement("p");
+                            newDC.innerHTML = "Nuevas muertes confirmadas: " + json.Countries[i].NewDeaths;
+                            document.getElementById("alignDiv").appendChild(newDC);
+    
+                            let newTD = document.createElement("p");
+                            newTD.innerHTML = "Total casos confirmados: " + json.Countries[i].TotalDeaths;
+                            document.getElementById("alignDiv").appendChild(newTD);
+    
+                            let newRC = document.createElement("p");
+                            newRC.innerHTML = "Nuevos casos recuperados : " + json.Countries[i].NewRecovered;
+                            document.getElementById("alignDiv").appendChild(newRC);
+    
+                            let newTR = document.createElement("p");
+                            newTR.innerHTML = "Total casos confirmados: " + json.Countries[i].TotalRecovered;
+                            document.getElementById("alignDiv").appendChild(newTR);
+    
+                            let espaciado = document.createElement("p");
+                            espaciado.innerHTML = "---------------------------------------------";
+                            document.getElementById("alignDiv").appendChild(espaciado);
+                        }
+                    }  
+                    parrafoTexto.innerHTML = "Ultima actualizacion: " + json.Date;
+                }
+            }
+            catch {
+                parrafoTexto.innerHTML = "Error al obtener los datos. Intente de nuevo mas tarde" ;
+            }
+
+        }
         else {
-            document.getElementById("texto").innerHTML =
-             "Por favor escriba bien el codigo del pais.<br> Si no lo conoces puedes encontrarlo en: https://www.iban.com/country-codes"
+            parrafoTexto.innerHTML =
+             "Por favor escriba bien el codigo del pais.<br> Si no lo conoces puedes encontrarlo en: https://www.iban.com/country-codes";
         }
         
     }
